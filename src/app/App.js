21f80a06a91b4a236a1c921ad8840ab1bd195056
Prawito <prawito.id@gmail.com>
2021-12-14 03:33:31 +0700

    gh-pages

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,7 @@ import Scroll from "../components/Header/Scroll";
 function App() {
     return (
         <GlobalProvider>
-            <BrowserRouter basename="/disney-id">
+            <BrowserRouter>
                 <Route
                     render={(props) => (
                         <>
